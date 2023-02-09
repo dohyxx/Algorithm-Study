5a60e71063bdc9cdb3253526ff7e034a0c373a43
Yein Park <yein1027@daum.net>
2023-02-09 17:20:18 +0900

    [BOJ, 1157] 단어 공부

--- yein/src/string/BOJ1157.java
+++ yein/src/string/BOJ1157.java
@@ -5,47 +5,33 @@ import java.util.*;
 
 public class BOJ1157 {
     public static void main(String[] args) throws IOException {
+            // 참고하여 수정
+
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            String word = br.readLine();
-            String[] wordTestArr = word.toUpperCase().split("");
-            String[] wordArr = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+            String word = br.readLine().toUpperCase();
 
-            HashMap<String, Integer> map = new HashMap<String, Integer>();
+            int[] wordArr = new int[26];
 
-            for(int i=0; i<wordTestArr.length;i++){
-                for(int j=0; j<wordArr.length; j++){
-                    if(wordTestArr[i].equals(wordArr[j])){
-                        if (map.containsKey(wordTestArr[i])) {
-                            map.replace(wordTestArr[i], (map.get(wordTestArr[i])+1));
-                        }else{
-                            map.put(wordTestArr[i],1);
-                        }
-                    }
+            for(int i=0; i<word.length(); i++){
+                    int numChk = word.charAt(i)-65;
+                    wordArr[numChk]++;
+            }
+
+            int max = -1;
+            char result='?';
+
+            for(int j=0; j<wordArr.length;j++){
+
+                if(wordArr[j] > max){
+                    max = wordArr[j];
+                    result = (char) (j + 65);
+                }else if(max == wordArr[j]){
+                    result='?';
                 }
             }
 
-            for(String key : map.keySet()){
-                int value = (Integer) map.get(key);
-            }
+        System.out.println(result);
 
-            Integer maxValues = Collections.max(map.values());
-
-            int totCnt = 0;
-            String result="?";
-            for(Map.Entry<String, Integer> entry : map.entrySet()){
-
-                if(entry.getValue().equals(maxValues)){
-                    totCnt++;
-                    result = entry.getKey();
-                }
-
-            }
-
-            if(totCnt > 1){
-                result = "?";
-            }
-
-            System.out.println(result);
     }
 }
